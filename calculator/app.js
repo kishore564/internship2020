46bd90d1b41dd93dfa3fbe4c2db049ad2f5d5b52
unknown <radhapanduri@gmail.com>
2020-06-05 19:12:42 +0530

    operators fixed

--- calculator/app.js
+++ calculator/app.js
@@ -1,22 +1,36 @@
 function c(val)
 {
-    
 document.getElementById("d").value=val;
 }
+
 function v(val)
 {
-  
+  var x = document.getElementById("d").value;
+  var lastChar=x.charAt(x.length-1);
+  if (!isNaN(val) || !isNaN(lastChar)) {
 document.getElementById("d").value+=val;
+  } else {
+   x = x.slice(0,-1) + val;
+    document.getElementById("d").value = x;
+  }
+}
+function d(val){
+document.getElementById("d").value = val;
 }
 function e() 
 { 
 try 
 { 
-   
-  c(eval(document.getElementById("d").value)) 
+  var x = document.getElementById("d").value;
+  var lastChar=x.charAt(x.length-1);
+  if (isNaN(lastChar)){
+    x = x.slice(0,-1);
+  }
+  console.log(x);
+  c(eval(x));
 } 
 catch(e) 
 {
   c('Error') 
 } 
-}  
+}
